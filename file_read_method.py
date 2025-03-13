fb4d23310d007299e03f79ae2c20243d47e18c2f
maksim <maksimfedorenko71@gmail.com>
2025-03-13 22:14:19 +0200

    finished file read method refactoring

--- file_read_method.py
+++ file_read_method.py
@@ -1,23 +1,32 @@
 import sys
 
-def solve_quadratic_from_file():
+def file_read_method():
     if len(sys.argv) != 2:
         print("Usage: python script.py <filename>")
         sys.exit(1)
 
     filename = sys.argv[1]
+
     try:
         with open(filename, 'r') as file:
-            data = file.read().strip()
-            parts = data.split()
+            data = file.read()
+
+            # Перевірка на наявність нового рядка в кінці файлу
+            if not data.endswith("\n"):
+                raise ValueError
+
+            # Видаляємо зайві пробіли та розбиваємо рядок на числа
+            parts = data.strip().split()
+
             if len(parts) != 3:
-                print("invalid file format")
-                sys.exit(1)
+                raise ValueError
 
             a, b, c = map(float, parts)
+
             if a == 0:
                 print("Error. a cannot be 0")
                 sys.exit(1)
+
     except FileNotFoundError:
         print(f"file {filename} does not exist")
         sys.exit(1)
@@ -26,6 +35,7 @@ def solve_quadratic_from_file():
         sys.exit(1)
 
     print(f"Equation is: ({a}) x^2 + ({b}) x + ({c}) = 0")
+
     discriminant = b ** 2 - 4 * a * c
 
     if discriminant > 0:
@@ -37,9 +47,8 @@ def solve_quadratic_from_file():
     elif discriminant == 0:
         x = -b / (2 * a)
         print("There are 1 roots")
-        print(f"x1 = {x}")
+        print(f"x1 = {abs(x)}")
     else:
         print("There are 0 roots")
 
-if __name__ == "__main__":
-    solve_quadratic_from_file()
+file_read_method()
